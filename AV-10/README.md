67504d7f72cd108555ed9d5b4a1710126fbfb646
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-11 17:06:08 -0300

    Criando repositório com base no código do projeto pessoal, FitPlain

---
+++ AV-10/README.md
@@ -0,0 +1,27 @@
+## Protótipos de Telas
+
+Nesse arquivo apresento os protótipos de telas da aplicação, sendo um exemplo o modelo no Figma e o outro no código.
+
+## Tela de Splash
+
+<img src="assets/readme/fitPlan_splashscreen.png" width="150"> 
+<img src="assets/readme/fitPlan_splashscreen-figma.png" width="150">
+
+## Tela de Login
+
+<img src="assets/readme/fitPlan_login.png" width="150"> 
+<img src="assets/readme/fitPlan_login-figma.png" width="150">
+
+## Tela de Cadastro
+
+<img src="assets/readme/fitPlan_cadastro.png" width="150"> 
+<img src="assets/readme/fitPlan_cadastro-figma.png" width="150">
+
+## Tela Inicial (Home)
+
+<img src="assets/readme/fitPlan_home.png" width="150"> 
+<img src="assets/readme/fitPlan_home-figma.png" width="150">   
+
+## Continuação do Projeto
+
+A continuação desse projeto será realizada em: [Link para outro repositório](https://github.com/PRoberto04/projDevMob)
---
+++ AV-10/lib/views/cadastro_page.dart
@@ -0,0 +1,106 @@
+import 'package:fit_plain/views/home_page.dart';
+import 'package:fit_plain/views/login_page.dart';
+import 'package:flutter/material.dart';
+
+
+class CadastroPage extends StatelessWidget {
+  const CadastroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Fit Plan',
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.w300,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Image.asset(
+            './assets/images/haltere-icon.png',
+            alignment: Alignment.topCenter,
+            height: 126,
+            width: 281,
+          ),
+          const Text(
+            'Cadastro',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w500, // Peso da fonte
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          const SizedBox(height: 15),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Jonathan Joestar',
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'exemplo@gmail.com',
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'exemplosenha123',
+                  ),
+                  obscureText: true,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 60.0),
+            child: SizedBox(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Lógica de autenticação
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                            builder: (context) => const HomePage()),
+                      );
+                    },
+                    child: const Text(
+                      'Entrar',
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => const LoginPage()),
+              );
+            },
+            child: const Text('Já tem uma conta? Clique aqui'),
+          )
+        ],
+      )),
+    );
+  }
+}
